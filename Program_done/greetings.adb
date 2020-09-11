b0559f44071d3989266d1d426a6c278670653837
KTH Student <student@il2206.vm>
2020-09-11 14:40:46 +0000

    Ordine nella cartella

---
+++ Program_done/greetings.adb
@@ -0,0 +1,14 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+package body Greetings is 
+	procedure Hello is
+	begin
+		Put_line ("Hello WORLD!");
+	end Hello;
+
+	procedure Goodbye is
+	begin
+		Put_Line ("Goodbye WORLD!");
+	end GoodBye;
+end Greetings;
+
