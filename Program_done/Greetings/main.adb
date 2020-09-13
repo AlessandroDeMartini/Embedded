b0bff12c63c1d6b30d1466d5e2d9c091b4b192e5
KTH Student <student@il2206.vm>
2020-09-13 11:04:54 +0000

    Buffer last version

--- Program_done/Greetings/main.adb
+++ Program_done/Greetings/main.adb
@@ -3,9 +3,9 @@ with Greetings;
 with Semaphores;
 
 procedure main is
+
 begin
 	Greetings.Hello;
 	Greetings.Goodbye;
-	--Semaphores.Wait;
-	--Semaphores.Signal;
+	
 end main;
--- Program_done/producerconsumer_prot.adb
+++ Program_done/producerconsumer_prot.adb
@@ -4,6 +4,8 @@ use Ada.Text_IO;
 with Ada.Real_Time;
 use Ada.Real_Time;
 
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
 with Buffer;
 use Buffer;
 
@@ -12,7 +14,7 @@ with Ada.Numerics.Discrete_Random;
 procedure ProducerConsumer_Prot is
 
    	N : constant Integer := 10; -- Number of produced and consumed tokens per task
-   	X : constant Integer := 3; -- Number of producers and consumers
+   	X : constant Integer := 3;  -- Number of producers and consumers
 
 	-- Random Delays
    	subtype Delay_Interval is Integer range 50..250;
@@ -21,9 +23,9 @@ procedure ProducerConsumer_Prot is
    	G : Generator;
 
    	-- Buffer definition
-    CB: CircularBuffer; --definition of a name for the circular buffer
+    CB: CircularBuffer;          --definition of a name for the circular buffer
     Size: constant Integer := 3; -- size of 3 so that all producer insert 1 
-  	subtype Item is Integer; -- all the rest of the definition of the buffer
+  	subtype Item is Integer;     -- all the rest of the definition of the buffer
 
  	type Index is mod Size;
 	package Index_IO is 
@@ -39,25 +41,16 @@ procedure ProducerConsumer_Prot is
    	task type Consumer;
    
    	task body Producer is
-    	Next : Time; -- define next as a time
+    	Next : Time;   -- define next as a time
    	begin
     	Next := Clock; -- assign at the time next the value clock that will be implemented later
 
-		Put("First value of type Index_IO: ");
-   		Index_IO.Put(Index'First, 1);
-	   	Put_Line(" ");
-   
-	   	Put("Last value of type Index_IO: ");	
-	   	Index_IO.Put(Index'Last, 1);
-	   	Put_Line(" ");
-
 		for I in 1..N loop
 			
-        	-- ==> Complete code: Write to Buffer
-         
-       	 	CB.Put(I); -- write on the buffer CB inserting the value I
-         	Put_Line(Standard.Integer'Image(I)); 
-	 	
+        	-- write on the buffer CB inserting the value I
+       	 	CB.Put(I);
+         	Put_Line("Produce giving: "&I'Img); 
+	 		
          	-- Next 'Release' in 50..250ms
          	Next := Next + Milliseconds(Random(G));
          	delay until Next;
@@ -70,21 +63,19 @@ procedure ProducerConsumer_Prot is
    begin
       Next := Clock;
       for I in 1..N loop
-         -- Read from X
-			
-         -- ==> Complete code: Read from Buffer
-        CB.Get(X); -- read on the buffer CB on the value I
+        -- Read from X on the buffer CB
+        
+        CB.Get(X); 
+		Put_Line("Consume taking: "&X'Img);
 		
-		-- Put_Line(Standard.Integer'Image(I));	
-
-        --Put_Line(Integer'Image(X));
-        --Next := Next + Milliseconds(Random(G));
-        --delay until Next;
+        Next := Next + Milliseconds(Random(G));
+        delay until Next;
       end loop;
    end;
 	
 	-- Parallelize the work between different producer/costumer
-	P: array (Integer range 1..X) of Producer; -- both P and C have to run X time so the number of producer
+	-- both P and C have to run X time so the number of producer
+	P: array (Integer range 1..X) of Producer;
 	C: array (Integer range 1..X) of Consumer;
 	
 begin -- main task
